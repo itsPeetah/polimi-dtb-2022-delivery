858d4cbd0635e7875cee094099c88906b209d5ba
itsPeetah <pietro.g.moroni@gmail.com>
2022-12-12 00:00:55 +0100

    Styling and layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,17 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="w-full h-screen bg-gradient-to-br from-sky-900 to-slate-600 | flex flex-col items-center justify-center text-white">
+    <div className="w-full h-screen bg-gradient-to-br from-sky-900 to-slate-600 | flex flex-col items-center justify-center p-4 | text-center text-white">
       <h2 className="text-lg font-mono">DTB '22 - Cluster 4, Circle 2</h2>
       <h1 className="text-4xl">ReHired Temporary Exhibition</h1>
+      <p className="block sm:hidden text-amber-500 font-bold border-2 border-amber-500 rounded-lg my-4">
+        If the page is not displaying correctly on mobile, please consider
+        checking it out on your computer.
+      </p>
       <iframe
         className=" shadow-2xl m-4"
-        width="1120"
-        height="630"
+        width="800"
+        height="480"
         src="https://www.youtube.com/embed/aqm001a07Lo"
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
