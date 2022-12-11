3f2453fa527e654fbcd8b1d9b8124bd4c5d213ee
itsPeetah <pietro.g.moroni@gmail.com>
2022-12-11 23:51:19 +0100

    Webpage v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full h-screen bg-gradient-to-br from-sky-900 to-slate-600 | flex flex-col items-center justify-center text-white">
+      <h2 className="text-lg font-mono">DTB '22 - Cluster 4, Circle 2</h2>
+      <h1 className="text-4xl">ReHired Temporary Exhibition</h1>
+      <iframe
+        className=" shadow-2xl m-4"
+        width="1120"
+        height="630"
+        src="https://www.youtube.com/embed/aqm001a07Lo"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      ></iframe>
     </div>
   );
 }
